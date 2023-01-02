e54480350a76d063a557b6cc501feac1864b4113
Trilokesh Rangam <rangam.lokesh@gmail.com>
2023-01-03 00:30:08 +0530

    lfs tracking enabled
    Signed-off-by: Trilokesh Rangam <rangam.lokesh@gmail.com>

--- coverage.sh
+++ coverage.sh
@@ -1,17 +1,3 @@
-#!/bin/bash -e
-
-cargo rustc --features map -- --test -C link-dead-code -Z profile -Z no-landing-pads
-
-LCOVOPTS=(
-  --gcov-tool llvm-gcov
-  --rc lcov_branch_coverage=1
-  --rc lcov_excl_line=assert
-)
-lcov "${LCOVOPTS[@]}" --capture --directory . --base-directory . \
-  -o target/coverage/raw.lcov
-lcov "${LCOVOPTS[@]}" --extract target/coverage/raw.lcov "$(pwd)/*" \
-  -o target/coverage/raw_crate.lcov
-
-genhtml --branch-coverage --demangle-cpp --legend \
-  -o target/coverage/ \
-  target/coverage/raw_crate.lcov
+version https://git-lfs.github.com/spec/v1
+oid sha256:57bd8ef3165e7f017da7addcd74b5be8e97042b0d1f5d67070adec814e8304e9
+size 511
